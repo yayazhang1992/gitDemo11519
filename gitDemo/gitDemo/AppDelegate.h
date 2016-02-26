1e99fe0df6f215c9112b8f1bdd97c2ee1411cf6d
zyy <1424695927@qq.com>
2016-02-26 14:09:49 +0800

    first comit

---
+++ gitDemo/gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by qianfeng1 on 26/2/16.
+//  Copyright © 2016年 张娅娅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
