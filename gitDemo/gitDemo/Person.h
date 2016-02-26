cabaaeb2c92887cf9c6a49396c1b676b232cc395
zyy <1424695927@qq.com>
2016-02-26 14:20:26 +0800

    work1 创建 person类

---
+++ gitDemo/gitDemo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  gitDemo
+//
+//  Created by qianfeng1 on 26/2/16.
+//  Copyright © 2016年 张娅娅. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ gitDemo/gitDemo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  gitDemo
+//
+//  Created by qianfeng1 on 26/2/16.
+//  Copyright © 2016年 张娅娅. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
